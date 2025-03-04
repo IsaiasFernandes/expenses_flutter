eebdc2c2526b7e28398195c524abf57a2a50cd9e
IsaiasFernandes <isaiasfernandesop@gmail.com>
2025-03-04 16:55:22 -0300

    ajuste na lista de cards

--- lib/presentation/views/home/card/cardAdpter.dart
+++ lib/presentation/views/home/card/cardAdpter.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 
 import '../../../../utils/CardUtils.dart';
 
-
 class CardAdapter extends Card {
   late final Transaction transaction;
   late final String formattedDate;
@@ -20,49 +19,42 @@ class CardAdapter extends Card {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Row(
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-            decoration: BoxDecoration(
-              border: Border.all(color: Theme.of(context).primaryColor, width: 2),
-              borderRadius: BorderRadius.circular(20),
-              color: Color(AppColors.colorPrimary),
-            ),
-            padding: EdgeInsets.all(10),
-            child: Text(
-              formattedTittle,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Colors.white,
+      elevation: 5,
+      margin: const EdgeInsets.symmetric(
+        vertical: 8,
+        horizontal: 5,
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          backgroundColor: Theme.of(context).primaryColor,
+          child: Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: FittedBox(
+              child: Text(
+                formattedTittle,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
               ),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            spacing: 8,
+        ),
+        title: Text(
+          transaction.title,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        subtitle: Container(
+          child: Row(
             children: [
-              Text(
-                transaction.title,
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              Row(
-                children: [
-                  Text(
-                    formattedPrice,
-                    style: TextStyle(color: Colors.grey.shade800),
-                  ),
-                  SizedBox(width: 16),
-                  Text(
-                    formattedDate,
-                    style: TextStyle(color: Colors.grey.shade800),
-                  ),
-                ],
-              ),
+              Text(formattedPrice),
+              SizedBox(width: 16),
+              Text(formattedDate),
             ],
           ),
-        ],
+        ),
       ),
     );
   }

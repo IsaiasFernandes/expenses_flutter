4312b0ecdfc57cd66083b6acb1fc4d979dbfbc3b
IsaiasFernandes <isaiasfernandesop@gmail.com>
2025-03-04 20:35:13 -0300

    ajjuste na função generateAcronym

--- lib/utils/CardUtils.dart
+++ lib/utils/CardUtils.dart
@@ -14,17 +14,24 @@ String formatValue(double value) {
 }
 
 String generateAcronym(String text) {
+  // Verifica se o texto é nulo ou vazio
+  if (text == null || text.trim().isEmpty) {
+    return '';
+  }
+
+  // Lista de stopwords (palavras comuns a serem ignoradas)
   final List<String> stopwords = [
     'de', 'da', 'do', 'dos', 'das', 'e', 'para', 'com', 'a', 'o', 'as', 'os',
     'um', 'uma', 'uns', 'umas', 'por', 'no', 'na', 'nos', 'nas'
   ];
 
-  List<String> words = text
-      .split(' ')
-      .where((word) => !stopwords.contains(word.toLowerCase()))
-      .toList();
-
-  String acronym = words.map((word) => word[0].toUpperCase()).join();
+  // Divide o texto em palavras, remove as stopwords e gera o acrônimo
+  String acronym = text
+      .split(' ') // Divide o texto em palavras
+      .where((word) => word.trim().isNotEmpty) // Remove palavras vazias
+      .where((word) => !stopwords.contains(word.toLowerCase())) // Remove stopwords
+      .map((word) => word[0].toUpperCase()) // Pega a primeira letra de cada palavra
+      .join(); // Junta as letras para formar o acrônimo
 
   return acronym;
 }
